f21df7e1700b4017fa2cae893d1523127b51cb98
center-coin <centercoinkr@gmail.com>
2022-11-16 11:36:10 +0500

    Applying changes to piepline

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ cd /var/www
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
-apt -y install nodejs npm
+sudo apt -y install nodejs npm
 
 #_Download_PM2
 npm install pm2@latest -g
